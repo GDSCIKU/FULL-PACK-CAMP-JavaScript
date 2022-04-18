14def2e584913fa09e3e92f2654bb5d9dfcb9dc2
issam_hosni <82038486+issam-hub@users.noreply.github.com>
2022-04-18 18:25:44 +0200

    Update week1.js
    adding some tips and fix something

--- week1.js
+++ week1.js
@@ -6,7 +6,7 @@ simple output: -4, 0, 5
 
 /* 2- Write a JavaScript program to sum the odds and evens under 100.*/
 
-/* 3- Write a JavaScript program to find the armstrong numbers of 3 digits
+/* 3- Write a JavaScript program that check if a number is an armstrong number , print true if so and false if not
 ps: an armstrong number is a number that if we add every digit on that number to the power of three , the result is the same number
 ex: 371 => 3**3 + 7**3 + 1**3 = 371
 
@@ -17,6 +17,10 @@ use .slice() method
 /* tips: 
 => you can display multiple things in console.log, for exemple console.log(a, b); will display a and b 
 => you can convert a number to string using .toString() method
+=> you can print a variable inside console.log using this syntax : 
+  ex:
+  let x = 5;
+  console.log(`x = ${x}`);
 */
 
 // Good luck Javascripters 🔥🔥
